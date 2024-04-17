e431bdf45120efa804cecf26b73a68d3d1b48a64
hyeonseo <80738573+kittyismylife@users.noreply.github.com>
2024-04-17 10:52:28 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
@@ -1,9 +1,13 @@
-def solution(a, b):
-    case1 = int(str(a)+str(b))
-    case2 = int(str(b)+str(a))
+# def solution(a, b):
+#     case1 = int(str(a)+str(b))
+#     case2 = int(str(b)+str(a))
     
-    if case1 < case2:
-        return case2
+#     if case1 < case2:
+#         return case2
 
-    else:
-        return case1
+#     else:
+#         return case1
+
+
+def solution(a, b):
+    return (max(int(f"{a}{b}"),int(f"{b}{a}")))
