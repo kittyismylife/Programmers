379d95d30123760421bff99caf5e07f5be05536e
hyeonseo <80738573+kittyismylife@users.noreply.github.com>
2024-04-17 10:46:36 +0900

    [level 0] Title: 문자열 섞기, Time: 0.00 ms, Memory: 9.93 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.py
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.py
@@ -1,12 +1,19 @@
+# def solution(str1, str2):
+#     mix1 = list(str1)
+#     mix2 = list(str2)
+#     mix3 = list(str1 + str2)
+#     answer = []
+    
+#     min_length = min(len(mix1), len(mix2))
+#     for i in range(min_length):
+#         answer.append(mix1[i])
+#         answer.append(mix2[i])
+    
+#     return ''.join(answer)
+
+
 def solution(str1, str2):
-    mix1 = list(str1)
-    mix2 = list(str2)
-    mix3 = list(str1 + str2)
-    answer = []
-    
-    min_length = min(len(mix1), len(mix2))
-    for i in range(min_length):
-        answer.append(mix1[i])
-        answer.append(mix2[i])
-    
-    return ''.join(answer)
+    answer = ''
+    for i in range(len(str1)):
+        answer += str1[i] + str2[i]
+    return answer
