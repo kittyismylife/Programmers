21a1df0f3b422a21829082fa912da520d2da814d
hyeonseo <80738573+kittyismylife@users.noreply.github.com>
2024-04-27 15:21:24 +0900

    Update 문자열 내 p와 y의 개수.py

--- 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.py
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.py
@@ -1,6 +1,6 @@
 def solution(s):
-    s = s.lower()  # 문자열을 소문자로 변환
-    count_p = s.count('p')  # 'p'의 개수 세기
-    count_y = s.count('y')  # 'y'의 개수 세기
+    s = s.lower()  # 소문자와 대문자를 구별하지 않음
+    count_p = s.count('p')
+    count_y = s.count('y')
 
     return count_p == count_y
